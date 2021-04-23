435a72311fa747356beb6f1d0132fadd0ab7aac9
yueyue200830 <yueyue200830@163.com>
2021-04-23 18:16:21 +0800

    Add video demo

---
+++ src/components/VideoUpload.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-upload
+      :action="fileUploadURL"
+      :on-success="handleImageUploadSuccess"
+      :file-list="fileList"
+      :show-file-list="false"
+      list-type="text"
+  >
+    <el-button size="small" type="primary">点击上传</el-button>
+  </el-upload>
+</template>
+
+<script>
+export default {
+  name: "VideoUpload",
+  data() {
+    return {
+      fileUploadURL: '/api/tool/file',
+      fileList: [],
+    }
+  },
+  methods: {
+    handleImageUploadSuccess: function () {
+      this.$message.success('upload success')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
